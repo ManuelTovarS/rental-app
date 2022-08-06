241abbab6770a043b073f02c507093b50b915b9b
Manuel J. Tovar <manuel.tovar@veevart.com>
2022-08-06 18:38:26 -0500

    Reservation page

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,8 +1,137 @@
-import React from 'react'
+import React from "react";
+import "./styles/Registration.css";
 
 export const Registration = () => {
   return (
-    <div>registration</div>
-  )
-}
+    <div className="payment-wrapper">
+      <div className="payment-content">
+        <div className="payment-container1 border-card">
+          <div className="location-image">
+            <img src="https://static5.depositphotos.com/1007593/453/i/450/depositphotos_4530089-stock-photo-african-landscape.jpg" />
+          </div>
 
+          <h3>Package Information</h3>
+
+          <h4>Street 54-23</h4>
+          <p>Tuesday April 8th, 2022 - 7:00am - 8:00am</p>
+
+          <div className="capacity-info">
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.7181 14.5813C16.6157 13.5013 17.24 12.2211 17.5381 10.8489C17.8363 9.47664 17.7996 8.05279 17.4311 6.69777C17.0625 5.34274 16.3731 4.0964 15.421 3.06418C14.469 2.03196 13.2823 1.24423 11.9614 0.767622C10.6405 0.291016 9.22428 0.139556 7.83248 0.326056C6.44067 0.512556 5.11426 1.03153 3.96545 1.83907C2.81664 2.64662 1.87922 3.71897 1.2325 4.96543C0.585774 6.21188 0.248773 7.59576 0.250003 9.00001C0.250475 11.0414 0.969844 13.0174 2.28188 14.5813L2.26938 14.5919C2.31313 14.6444 2.36313 14.6894 2.40813 14.7413C2.46438 14.8056 2.525 14.8663 2.58313 14.9288C2.75813 15.1188 2.93813 15.3013 3.12688 15.4725C3.18438 15.525 3.24375 15.5738 3.30188 15.6238C3.50188 15.7963 3.7075 15.96 3.92063 16.1125C3.94813 16.1313 3.97313 16.1556 4.00063 16.175V16.1675C5.46444 17.1976 7.21069 17.7505 9.00063 17.7505C10.7906 17.7505 12.5368 17.1976 14.0006 16.1675V16.175C14.0281 16.1556 14.0525 16.1313 14.0806 16.1125C14.2931 15.9594 14.4994 15.7963 14.6994 15.6238C14.7575 15.5738 14.8169 15.5244 14.8744 15.4725C15.0631 15.3006 15.2431 15.1188 15.4181 14.9288C15.4763 14.8663 15.5363 14.8056 15.5931 14.7413C15.6375 14.6894 15.6881 14.6444 15.7319 14.5913L15.7181 14.5813ZM9 4.00001C9.55626 4.00001 10.1 4.16496 10.5625 4.474C11.0251 4.78304 11.3855 5.22229 11.5984 5.73621C11.8113 6.25013 11.867 6.81563 11.7585 7.3612C11.6499 7.90677 11.3821 8.40791 10.9887 8.80124C10.5954 9.19458 10.0943 9.46244 9.5487 9.57096C9.00312 9.67949 8.43762 9.62379 7.92371 9.41092C7.40979 9.19805 6.97054 8.83756 6.6615 8.37505C6.35245 7.91253 6.1875 7.36877 6.1875 6.81251C6.1875 6.06659 6.48382 5.35121 7.01127 4.82377C7.53871 4.29632 8.25408 4.00001 9 4.00001ZM4.00438 14.5813C4.01522 13.7606 4.34872 12.9772 4.93273 12.4006C5.51673 11.824 6.30429 11.5004 7.125 11.5H10.875C11.6957 11.5004 12.4833 11.824 13.0673 12.4006C13.6513 12.9772 13.9848 13.7606 13.9956 14.5813C12.6249 15.8165 10.8452 16.5001 9 16.5001C7.15484 16.5001 5.3751 15.8165 4.00438 14.5813Z"
+              fill="black"
+            />
+          </svg>
+          <p>Capacity: 30</p>
+          </div>
+
+          <div className="location-info">
+            <p>Description</p>
+          </div>
+
+          <div className="justify-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, per inceptos himenaeos. Praesent auctor purus
+              luctus enim egestas, ac scelerisque ante pulvinar. Donec ut
+              rhoncus ex.
+            </p>
+          </div>
+
+          <div className="location-info">
+            <p>Disclaimers</p>
+          </div>
+
+          <div className="justify-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, per inceptos himenaeos. Praesent auctor purus
+              luctus enim egestas, ac scelerisque ante pulvinar. Donec ut
+              rhoncus ex.
+            </p>
+          </div>
+          <div className="location-info">
+            <p>Insurance Information</p>
+          </div>
+          <div className="justify-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, per inceptos himenaeos. Praesent auctor purus
+              luctus enim egestas, ac scelerisque ante pulvinar. Donec ut
+              rhoncus ex.
+            </p>
+          </div>
+        </div>
+
+        <div className="payment-container2 border-card">
+          <div className="personal-information">
+            <h3 className="title-sections">Host Information</h3>
+
+            <div className="two-columns">
+              <div className="input-box-half">
+                <p>
+                  <b className="require-field">*</b>First Name
+                </p>
+                <input type="text" />
+              </div>
+
+              <div className="input-box-half">
+                <p>
+                  <b className="require-field">*</b>Last Name
+                </p>
+                <input type="text" />
+              </div>
+            </div>
+
+            <div className="one-columns">
+              <div className="input-box-all">
+                <p>
+                  <b className="require-field">*</b>Email
+                </p>
+                <input type="text" />
+              </div>
+            </div>
+
+            <div className="one-columns">
+              <div className="input-box-all">
+                <p>
+                <b className="require-field">*</b>Home Phone</p>
+                <input type="text" />
+              </div>
+            </div>
+
+            <div className="one-columns">
+              <div className="input-box-all">
+                <p>
+                <b className="require-field">*</b>Mobile Phone</p>
+                <input type="text" />
+              </div>
+            </div>
+
+            <div className="input-box-all">
+              <button>Go to payment </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
